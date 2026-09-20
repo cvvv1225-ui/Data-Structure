f3179225f546d2f6f600981bfc076a93d6c6491b
cvvv1225-ui <cvvv1225@gmail.com>
2026-09-20 21:16:28 +0900

    Rename 2-9.cpp to 2-09.cpp

--- 2-09.cpp
+++ 2-09.cpp
@@ -5,7 +5,7 @@ int main() {
 	char string1[20] = "Dreams come true!", string2[20], *ptr1, *ptr2;
 	
 	ptr1 = string1;
-	printf("\n string1¿« ¡÷º“ = %u \t ptr1 = %u", string1, ptr1);
+	printf("\n string1√Ä√á √Å√ñ¬º√í = %u \t ptr1 = %u", string1, ptr1);
 	printf("\n string1 = %s \n ptr1 = %s", string1, ptr1);
 	printf("\n\n %s", ptr1 + 7);
 	ptr2 = &string1[7];
